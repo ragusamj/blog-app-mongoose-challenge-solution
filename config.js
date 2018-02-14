e2d0e2fefa6c2ef9ab29ec37a1d3a0b0a58007dd
Marija Jovanovic <marija.jo@hotmail.com>
2018-02-13 19:33:33 -0500

    initial push

--- config.js
+++ config.js
@@ -3,8 +3,10 @@
 require('dotenv').config();
 
 exports.DATABASE_URL = process.env.DATABASE_URL ||
-                       global.DATABASE_URL ||
-                      'mongodb://localhost/blog-app';
+    global.DATABASE_URL ||
+    'mongodb://localhost/blog-app';
 exports.TEST_DATABASE_URL = (process.env.TEST_DATABASE_URL ||
-                            'mongodb://localhost/test-blog-app');
+    'mongodb://localhost/test-blog-app');
 exports.PORT = process.env.PORT || 8080;
+
+//
